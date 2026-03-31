a8feca1a26ea63bf46cddf278568cbab06d12dd9
Mobel_Coding <azizbeknurmuhammadov747@gmail.com>
2026-03-31 23:27:46 +0500

    Refactor soz function to preserve spaces

---
+++ main.py
@@ -0,0 +1,59 @@
+#1--
+
+# def son(a):
+#     return sum(int(i) for i in str(a))
+#
+# print(son(int(input())))
+
+#2--
+# names = ['ali', 'vali', 'hoshim', 'aziz']
+# def full_name(names):
+#     return [name.capitalize() for name in names]
+#
+# # names = str(input())
+# print(full_name(names))
+
+#3--
+
+# score = [66, 90, 68, 59, 76, 60, 88, 74, 81, 65]
+#
+# def scores(score):
+#     return [a for a in score if a > 75]
+#
+# print(scores(score))
+
+#4-
+
+#5--
+def soz(text):
+    i = 0
+    result = ""
+
+    while i < len(text):
+        if text[i] == 'e':
+            result += '3'
+        else:
+            result += text[i]
+        i += 1
+
+    print(result)
+
+text = input("tekst kiriting: ")
+print(soz(text))
+
+#6--
+
+def soz(text):
+    i = 0
+    result = ""
+
+    while i< len(text):
+        if text[i] == " ":
+            result += text[i]
+        i += 1
+    print(result)
+
+text = input("tekst kiriting: ")
+print(soz(text))
+
+#7--
